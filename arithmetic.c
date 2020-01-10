c40d35e26bdef57b67bfa423d5779ae07626d572
FahimMuntashir <fahim.tech.me@gmail.com>
2020-01-10 18:02:31 +0600

    Upload

---
+++ arithmetic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main(){
+
+    int a,b,c;
+    printf("enter The first number:");
+    scanf("%d", &a);
+    printf("enter the second num :");
+
+    scanf("%d", &b);
+
+    //c=a+b;
+
+    printf("The sum is : %d\n", a+b);
+    printf("Subtraction is : %d\n", a-b);
+    printf("Multiplication is : %d\n", a*b);
+    printf("Division is : %d\n", a/b);
+
+
+}
