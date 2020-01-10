7936a6cadcaa4df96dd37b9ba270fb436569adff
FahimMuntashir <fahim.tech.me@gmail.com>
2020-01-10 18:30:04 +0600

    Odd and Even

---
+++ odd and even.c
@@ -0,0 +1,25 @@
+/*
+ Author : Fahim Muntashir
+ mail : fahim.tech.me@gmail.com
+*/
+
+
+
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    if (a%2==0)
+    {
+        printf("The Number is even : %d\n", a);
+    }
+    else
+    {
+        printf("The number is odd: %d\n", a);
+    }
+    
+    
+    
+}
