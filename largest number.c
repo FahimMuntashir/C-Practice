0164051555063ecaab17b66de9b41e4bc720f9b6
FahimMuntashir <fahim.tech.me@gmail.com>
2020-01-10 18:12:30 +0600

    Largest number

---
+++ largest number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+
+  int a,b;
+  printf("Enter the first num  :");
+  scanf("%d", &a);
+  printf("enter the second num :");
+  scanf("%d", &b);
+
+  if(a>b){
+
+      printf(" The first num is largest : %d\n ", a);
+  }
+  else
+  {
+       printf(" the Second num is the largest: %d\n", b);
+
+  }
+  
+
+}
