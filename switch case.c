02246ce655f5f74ce6ca12416966dc48b7d3253f
FahimMuntashir <fahim.tech.me@gmail.com>
2020-01-11 14:59:09 +0600

    Switch Case Even Odd

---
+++ switch case.c
@@ -0,0 +1,26 @@
+/*
+ Author : Fahim Muntashir
+ mail : fahim.tech.me@gmail.com
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+    int number;
+     
+    printf("Enter a  number: ");
+    scanf("%d",&number);
+     
+    switch(number%2) //this will return either 0 or 1
+    {
+        case 0:
+            printf("%d is an EVEN number.\n",number);
+            break;
+        case 1:
+            printf("%d is an ODD number.\n",number);
+            break;
+    }
+     
+    return 0;
+}
